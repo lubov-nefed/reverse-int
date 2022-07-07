797a34b6b801485ed0ddaed0bf930bd053de9c7a
lubov-nefed <love.nefed@gmail.com>
2022-07-07 10:22:19 +0300

    init: start reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) n *= -1;
+  let string = String(n);
+  let newString = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString = newString + string[i];
+  }
+  return newString;
 }
